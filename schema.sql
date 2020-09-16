8ab007c7ec29db59d6181a7a1f32dba06fe2bcf1
Chris Perrins <chris.perrins@vix.digital>
2020-09-16 15:20:38 +0100

    schema file added for endpoints, endpoint_history and information

---
+++ schema.sql
@@ -0,0 +1,7 @@
+create database if not exists pollervertx;
+
+create table if not exists endpoints (id int primary key auto_increment, name varchar(50), endpoint text, frequency int, active boolean);
+
+create table if not exists endpoint_history(id int primary key auto_increment, endpoint_id int, status varchar(3), response_time int, timedate datetime, foreign key(endpoint_id) references endpoints(id));
+
+create table if not exists information(id int primary key auto_increment, endpoint_id int, owner varchar(40), street text, postcode varchar(10), city varchar(40), foreign key(endpoint_id) references endpoints(id));
